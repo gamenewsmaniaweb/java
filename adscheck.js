351f5872018aa0b7cd169649b69592fbfb23feda
Game News Mania <94456855+gamenewsmaniaweb@users.noreply.github.com>
2023-04-08 20:18:07 +0530

    Create adscheck.js

---
+++ adscheck.js
@@ -0,0 +1,8 @@
+if(typeof adsbygoogle !== 'undefined'){
+  // AdSense script has loaded
+  alert("AdSense script has loaded successfully");
+  // Add your functionality here
+} else {
+  // AdSense script has not loaded
+  // Add fallback code here
+}
