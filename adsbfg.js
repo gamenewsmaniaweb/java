bc9328fae76666171bd7aa5ff7654d3c80b7a066
Game News Mania <94456855+gamenewsmaniaweb@users.noreply.github.com>
2023-04-08 20:59:22 +0530

    Create adsbfg.js

---
+++ adsbfg.js
@@ -0,0 +1,2 @@
+if(typeof adsbygoogle!=='undefined'){var intervalId=setInterval(function(){var adElement=document.querySelector('ins.mobBTAD');if(adElement.getAttribute('data-ad-status')==='filled'){document.getElementById("ADShowM").classList.add("v");clearInterval(intervalId)}
+elapsedSeconds++;if(elapsedSeconds>=5){clearInterval(intervalId)}},1000);var elapsedSeconds=0};
